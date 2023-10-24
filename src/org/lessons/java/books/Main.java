5c80b7c3687540cec15793ba4b556abefcb2f65b
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-24 14:26:48 +0200

    create new arrauy for books

--- src/org/lessons/java/books/Main.java
+++ src/org/lessons/java/books/Main.java
@@ -1,7 +1,13 @@
 package org.lessons.java.books;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // Istanzio lo scanner
+        Scanner scan = new Scanner(System.in);
 
+        //creo un array di oggetti di libri
+        Book[] libri = new Book[100];
     }
 }

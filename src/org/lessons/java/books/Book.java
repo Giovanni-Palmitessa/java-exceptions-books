62473561947ff45ce62350db476a1ade4d24bbc0
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-24 14:22:12 +0200

    add getter and setter for attributes

--- src/org/lessons/java/books/Book.java
+++ src/org/lessons/java/books/Book.java
@@ -14,4 +14,31 @@ public class Book {
         this.author = author;
         this.publisher = publisher;
     }
+
+    //GETTER E SETTER
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public int getPages() {
+        return pages;
+    }
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public String getPublisher() {
+        return publisher;
+    }
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
 }

f706d8b00c2e1822c29fdec84560da6538a1f22b
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-24 15:52:32 +0200

    rewrite main and remove controls

--- src/org/lessons/java/books/Main.java
+++ src/org/lessons/java/books/Main.java
@@ -13,60 +13,45 @@ public class Main {
         //indice per i libri
         int indice = 0;
 
-        //istanzio ciclo while per iterare su array dei libri
-        while (true) {
+        // ciclo while per chiedere all'utente di inserire un libro
+        while (indice < libri.length) {
             try {
-                // chiedere i dati del libro
-                System.out.println("Inserisci i dati del libro!");
+                System.out.println("Inserisci i dati del libro:");
 
-                // Titolo del Libro
-                System.out.println("Inserisci il titolo del libro: ");
+                // Chiedo il Titolo
+                System.out.print("Titolo: ");
                 String title = scan.nextLine();
-                if (title.isEmpty()) {
-                    throw new IllegalArgumentException("Il titolo non può essere vuoto!");
-                }
 
-                // Numero di pagine
-                int pages;
-                do {
-                    System.out.println("Inserisci il numero di pagine: ");
-                    try {
-                        pages = Integer.parseInt(scan.nextLine());
-                        if (pages <= 0) {
-                            throw new IllegalArgumentException("Non puoi inserire un numero di pagine inferiore a 0!");
-                        }
-                    } catch (NumberFormatException e) {
-                        System.out.println("Inserisci un numero valido per il numero di pagine!");
-                        pages = -1; // Imposta un valore non valido per continuare il ciclo
-                    }
-                } while (pages <= 0);
+                //Chiedo il numero di pagine
+                System.out.print("Numero di pagine: ");
+                int pages = Integer.parseInt(scan.nextLine());
 
-                // Autore
-                System.out.println("Inserisci il nome dell'autore: ");
+                // Chiedo l'Autore
+                System.out.print("Autore: ");
                 String author = scan.nextLine();
-                if (author.isEmpty()) {
-                    throw new IllegalArgumentException("Il nome dell'autore non può essere vuoto");
-                }
 
-                // Editore
-                System.out.println("Inserisci il nome dell'editore: ");
+                // Chiedo l'Editore
+                System.out.print("Editore: ");
                 String publisher = scan.nextLine();
-                if (publisher.isEmpty()) {
-                    throw new IllegalArgumentException("Il nome dell'editore non può essere vuoto!");
-                }
 
-                // istanzio il nuovo oggetto Book
-                Book book = new Book(title, pages, author, publisher);
-                // aggiungo l'oggetto appena creato all'array
-                libri[indice] = book;
-                // aumento il contatore per far ripartire il ciclo
+                // Crea un nuovo oggetto Book e inseriscilo nell'array
+                libri[indice] = new Book(title, pages, author, publisher);
                 indice++;
 
-                // se tutto è andato bene confermo l'inserimento del libro nell'array
-                System.out.println("Libro inserito correttamente");
+                System.out.println("Libro inserito correttamente.");
             } catch (IllegalArgumentException e) {
                 System.out.println("Errore: " + e.getMessage());
             }
+        }
+
+        // Stampa il catalogo dei libri
+        System.out.println("Catalogo dei libri:");
+        for (int i = 0; i < indice; i++) {
+            System.out.println("Titolo: " + libri[i].getTitle());
+            System.out.println("Numero di pagine: " + libri[i].getPages());
+            System.out.println("Autore: " + libri[i].getAuthor());
+            System.out.println("Editore: " + libri[i].getPublisher());
+            System.out.println();
         }
 
     }

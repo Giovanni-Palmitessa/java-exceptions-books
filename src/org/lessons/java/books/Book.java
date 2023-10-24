894db1b7ce7d5d08454cd5dd310171da594f8055
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-24 14:19:19 +0200

    add class Book and attributes

--- src/org/lessons/java/books/Book.java
+++ src/org/lessons/java/books/Book.java
@@ -3,4 +3,9 @@ package org.lessons.java.books;
 public class Book {
     //ATTRIBUTI
     private String title;
+    private int pages;
+    private String author;
+    private String publisher;
+
+
 }
